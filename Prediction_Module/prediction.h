4b56d4f58610c9b22bafb0d4004500c493c168f8
kk2491 <38481564+kk2491@users.noreply.github.com>
2018-06-23 21:26:33 +0530

    Create prediction.h

---
+++ Prediction_Module/prediction.h
@@ -0,0 +1,21 @@
+Header file for prediction.cc
+
+Class - Prediction which inherits from PredictionInterface
+
+public
+    destructor()
+    Name()
+    Init()
+    Start()
+    Stop()
+    RunOnce()
+
+
+Private
+    start_time = 0
+    Create a object "prediction_conf_" of class PredictionConf
+    Create a object "adapter_conf_" of class AdapterManagerConfig
+
+check the prediction.cc for function definition
+    
+
