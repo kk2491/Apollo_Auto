b5ba99d1f969fdc2913aa58ec1ab9f3c524c99cd
kk2491 <38481564+kk2491@users.noreply.github.com>
2018-06-24 09:17:59 +0530

    Update prediction.h

--- Prediction_Module/prediction.h
+++ Prediction_Module/prediction.h
@@ -1,14 +1,34 @@
 Header file for prediction.cc
 
+#define MODULES_PREDICTION_PREDICTION_H_  -- For this file
+
+Libraries included :    Proto files from locationlization, perception, planning, prediction
+                        modules/common/adapters/proto/adapter_config.pb.h
+                        modules/common/proto/pnc_point.pb.h
+                        modules/localization/proto/locationlization.pb.h
+                        modules/perception/proto/perception_obstacle.pb.h
+                        modules/planning/proto/planning.pb.h
+                        modules/prediction/prediction_interface.h
+                        modules/prediction/proto/prediction_conf.pb.h
+
+
 Class - Prediction which inherits from PredictionInterface
 
 public
     destructor()
-    Name()
-    Init()
-    Start()
-    Stop()
-    RunOnce()
+    Name()        -     Get the name of the ros node
+    Init()        -     Initialize the ros node
+    Start()       -     Start the node
+    Stop()        -     Stop the node
+    RunOnce()     -     Function runs upon receiving a perception obstacle
+
+
+private
+    Status OnError()    -     
+    OnLocalization      -       
+    OnPlanning          -   
+
+
 
 
 Private
