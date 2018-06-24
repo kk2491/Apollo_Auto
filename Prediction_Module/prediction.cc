56eaef8aaee95d58da397df9113537b57429cb22
kk2491 <38481564+kk2491@users.noreply.github.com>
2018-06-24 10:25:19 +0530

    Create prediction.cc

---
+++ Prediction_Module/prediction.cc
@@ -0,0 +1,26 @@
+
+Libraries included  :   modules/common/adapters/adapter_manager.h                    
+                        modules/common/math/vec2d.h
+                        modules/common/time/time.h
+                        modules/common/util/file.h
+                        modules/prediction/common/feature_output.h
+                        modules/prediction/common/prediction_gflags.h
+                        modules/prediction/common/prediction_map.h
+                        modules/prediction/common/validation_checker.h
+                        modules/prediction/container/container_manager.h
+                        modules/prediction/container/obstacles/obstacles_container.h
+                        modules/prediction/container/pose/pose_container.h
+                        modules/prediction/evaluator/evaluator_manager.h
+                        modules/prediction/predictor/predictor_manager.h
+                        modules/prediction/proto/prediction_obstacle.pb.h
+
+
+Function Definition for the functions in prediction.h
+
+Class Prediction
+
+Prediction::Name()
+    Params   - None
+    Returns  - FLAGS_prediction_module_name   -- Check where this is?
+    
+     
