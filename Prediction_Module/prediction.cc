2e73416b914906b9483c45a6de0a823e182d8f65
kk2491 <38481564+kk2491@users.noreply.github.com>
2018-06-25 18:00:29 +0530

    Update prediction.cc

--- Prediction_Module/prediction.cc
+++ Prediction_Module/prediction.cc
@@ -27,12 +27,81 @@ Predition::Init()
 {
     Params    - None
     Returns   - start_time - Calls Clock::NowInSeconds() - Which is there in modules/common/time/time.h (time.cc)
-  
+    
+    //prediction_conf_ - Location - /modules/prediction/proto/prediction_conf.proto
+    //adapter_conf_ - Location - /modules/common/adapters/proto/adapter_config.proto
 
     prediction_conf_.Clear();    --- This is a prediction_conf proto class or message (Check where this is)
                              --- This clears the existing message/data
     
-    if(! common::util::GetProto
+    //Below condition GetProtoFromFile(string &File_Name, MessageType *message) returns Bool
+    //GetProtoFromFile - Location - modules/common/util/file.h
+    //This actually checks below 2 functions 
+    //GetProtoFromASCIIFile - This checks if the file is openable and parsable
+    //GetProtoFromBinaryFile - This checks if the file is openable and parsable
+    
+    if (GetProtoFromFile(FLAGS_prediction_conf_file, &prediction_conf_)    //Check the filename - FLAGS_prediction_conf_file
+        Failed to load the file
+    else
+        File loaded - prediction_conf.ShortDebugString()
+    
+        
+    adapeter_conf_.Clear();
+    if (GetProtoFromFile(FLAGS_prediction_adapter_config_filename, &adapter_conf_)   //Check the filename - FLAGS_prediction_adapter_config_filename
+        Unable to load adapter conf file
+    else
+        Adapter config file loaded into adapeter_conf_.ShortDebugString() 
+        
+        
+        
+        
+    //Initialize all managers (Adapter, Container, Evaluator, Predictor)
+    
+    //Location - /modules/common/adapters/adapeter_config.proto
+    //adapter_conf_ -- Object of AdapterManagerConfig
+    //Adapter::Init -> This is in adapter_manager.cc 
+    AdapterManager::Init(adapter_conf_);
+        if adapter_config_.is_ros -> Create a new ros Nodehandle
+        Go though the message or topics one by one and enable -- check where Enable**** function is there
+    
+    //Registers the containers
+    //Check : Separate container will be created for each type ?  
+    //Location - /modules/prediction/container/container_manager.cc
+    //Init -> RegisterContainers() -> check condition -> RegisterContainer() -> CreateContainer()
+    ContainerManager::instance()->Init(adapter_conf_)
+    
+    
+    //Location - /modules/prediction/evaluator_manager.cc
+    //Go through the obstacles one by one (message obstacle_conf)
+        //Check if the obstacle type is defined (obstacle_conf.has_obstacle_type())
+        //Check if the obstacle evaluator type is defined (obstacle_conf.has_evaluator_type())
+        //Check if the obstacle has status and obstacle_status is OnLane
+            //Determine whether it is vehicle, cyclist, pedestrian or unknown
+        
+    EvaluatorManager::instance()->Init(prediction_conf_);
+    PredictorManager::instance()->Init(prediction_conf_);
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      
+    
+    
+      
+                 
 }
 
                              
